89ec68ad0758f8963f7ec093f146ffd55ce7543f
YAHYAOUI AYA <ayayahyaoui19@gmail.com>
2020-05-11 15:17:15 +0000

    Update kfold.py

--- src/kfold.py
+++ src/kfold.py
@@ -6,17 +6,41 @@ from keras.layers import Conv2D, MaxPool2D, Flatten
 from keras.layers import Dropout, Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
-from sklearn.utils.class_weight import compute_class_weight
 from sklearn.model_selection import KFold
-
 from tqdm import tqdm
 from python_speech_features import mfcc
 import pickle
 from keras.callbacks import ModelCheckpoint
 from cfg4 import config
-import matplotlib.pyplot as plt
+   
+csv_namefile = 'effets_guitare4.csv'  
+clean_namedir =  'clean4' 
 config = config()
 
+
+def Init (csv_namefile):
+                
+    # Téléchargement du fichier Excel qui contient le nom de la piste avec label qui le correspond       
+    df = pd.read_csv(csv_namefile)
+    df.set_index('fname', inplace=True)
+    
+    # Récuperer les échantions nettoyées et le calcul de la longeur de chaque piste
+    for f in df.index:
+        rate, signal = wavfile.read(clean_namedir+'/'+f)
+        df.at[f, 'length'] = signal.shape[0]/rate
+        
+    # Récupérer les labelles des classes : Chorus , Nickel-Power , Reverb - Phaser_ 
+    classes = list(np.unique(df.label))
+    class_dist = df.groupby(['label'])['length'].mean()
+    # Création des N sample , la probabilité de distribution 
+    n_samples = 2* int(df['length'].sum()/0.1)
+    prob_dist = class_dist / class_dist.sum()
+    # choices = np.random.choice(class_dist.index, p=prob_dist)
+
+    
+    return df, classes , class_dist , n_samples , prob_dist
+
+    
 # Verifier si un modele existe déja pour éviter la répétition du travail 
 def check_config():
     if os.path.isfile(config.p_path) :
@@ -49,6 +73,10 @@ def check_Kfold():
 # Creation des échantillons    
 def build_rand_feat(csv_namefile,clean_namedir):
     
+    #Initialiser les variables utilisées 
+    df, classes , class_dist , n_samples , prob_dist = Init(csv_namefile)
+
+    
     # tmp = check_config()
     # if tmp:
     #    return tmp.data[0], tmp.data[1]
@@ -57,23 +85,7 @@ def build_rand_feat(csv_namefile,clean_namedir):
     if samp :
         return samp[0], samp[1]
     
-    
-# Téléchargement du fichier Excel qui contient le nom de la piste avec label qui le correspond       
-    df = pd.read_csv(csv_namefile)
-    df.set_index('fname', inplace=True)
-    # Récuperer les échantions nettoyées et le calcul de la longeur de chaque piste
-    for f in df.index:
-        rate, signal = wavfile.read(clean_namedir+'/'+f)
-        df.at[f, 'length'] = signal.shape[0]/rate
-        
-    # Récupérer les labelles des classes : Chorus , Nickel-Power , Reverb - Phaser_ 
-    classes = list(np.unique(df.label))
-    class_dist = df.groupby(['label'])['length'].mean()
-    # Création des N sample , la probabilité de distribution 
-    n_samples = 2* int(df['length'].sum()/0.1)
-    prob_dist = class_dist / class_dist.sum()
-    # choices = np.random.choice(class_dist.index, p=prob_dist)
-
+ 
     #Pre-processing
     X=[]
     y=[]
@@ -81,7 +93,7 @@ def build_rand_feat(csv_namefile,clean_namedir):
     for _ in tqdm(range(n_samples)):
         rand_class= np.random.choice(class_dist.index,p=prob_dist)
         file = np.random.choice(df[df.label==rand_class].index)
-        rate , wav= wavfile.read('clean4/'+file)
+        rate , wav= wavfile.read(clean_namedir+'/'+file)
         label = df.at[file,'label']
         rand_index=np.random.randint(0,wav.shape[0]-config.step )
         sample = wav[rand_index:rand_index+config.step]
@@ -141,10 +153,12 @@ def get_conv_model(input_shape):
 
 # KFOLD method
 def get_score_KFOLD(model,X, y,nb_splits):
+    
+    
 # Vérifier s'il existe des donnés déjà préparés par Kfold pour meme X et y et nb_splits
     kf = check_Kfold()
     if kf :
-        if kf[1] == nb_splits and kf[2] == X and kf[3] == y :
+        if kf[1] == nb_splits and kf[2].all() == X.all() and kf[3].all() == y.all():
             return kf[0], kf[1]
     
     kf = KFold(n_splits = nb_splits)
@@ -155,13 +169,12 @@ def get_score_KFOLD(model,X, y,nb_splits):
 
         checkpoint = ModelCheckpoint(config.model_path, monitor='val_acc', verbose =1, mode ='max',
                          save_best_only=True, save_weights_only=False, period=1)
-        model.fit(train_X,train_y,epochs=10,batch_size=32,validation_data =(test_X , test_y) ,
+        model.fit(train_X,train_y,epochs=5,batch_size=32,validation_data =(test_X , test_y) ,
                   callbacks = [checkpoint])
         score =model.evaluate(test_X,test_y)
         scors_CNN.append(score)
         
-    data =[scors_CNN , nb_splits , X , y]
-    print(data)
+    data =(scors_CNN , nb_splits , X , y)
     
     with open(config.kfold_path , 'wb') as handle:
         pickle.dump(data, handle, protocol=2)  
@@ -173,21 +186,31 @@ def get_score_KFOLD(model,X, y,nb_splits):
 
 
 # La phase d'apprentissage 
-X , y = build_rand_feat('effets_guitare4.csv','clean4')
+X , y = build_rand_feat(csv_namefile,clean_namedir)
 input_shape = (X.shape[1],X.shape[2], 1 )
 model = get_conv_model(input_shape)
-Kfold , nb_splits = get_score_KFOLD(model,X,y,5)
+Kfold , nb_splits  = get_score_KFOLD(model,X,y,5)
 
 
-print('Scores :')
-print(Kfold)
+
+# Résultat acc et loss pour chaque itération(split =5)  + La moyenne de la précision 
+print('Scores :============================')
+for i in range(nb_splits):
+    print('Split numéro  :')
+    print(i)
+    print('loss :')
+    print(Kfold[i][0])
+    print('acc :')
+    print(Kfold[i][1])
+    print('====================================')
+
 
 somme = 0
 for i in range(nb_splits):
     somme += Kfold[i][1]
 
 Avg = somme /float(len(Kfold))
-print('Average score :' )
+print('\nAverage score :' )
 print(Avg *100)
 
 
