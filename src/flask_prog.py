131aeee14f11297ec020985538beded896f6f07f
YAHYAOUI AYA <ayayahyaoui19@gmail.com>
2020-06-02 14:54:51 +0100

    Update flask_prog.py

--- src/flask_prog.py
+++ src/flask_prog.py
@@ -4,14 +4,13 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/music', methods=['GET'])
-def plot_EMA():
+def player():
     try:
         return render_template("player.html",
                                wav='LaGrange-Guitars.wav'
                                ,title ="LaGrange-Guitars")
     except Exception as e:
         return str(e)
-
 
 
 if __name__ == "__main__":
