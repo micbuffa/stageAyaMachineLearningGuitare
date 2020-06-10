c23840d39fdb6149b66e422f7f04a4e9f52c4263
YAHYAOUI AYA <ayayahyaoui19@gmail.com>
2020-06-10 13:05:45 +0100

    Update predict_test.py

--- src/predict_test.py
+++ src/predict_test.py
@@ -14,7 +14,9 @@ from cfg4 import config
 #Initialiser les vaiables utilisées dans les fonctions
 csv_namefile2 = 'effets_guitare.csv'#le fichier excel des classes utilisées pour former le modèle
 csv_namefile = 'LaGrange-Guitars.csv' #le fichier excel de wavfile de test 
-clean_namedir =  'Test/clean_test' #le chemin de dossier de wavfile nettoyé de la classe de test
+# clean_namedir =  'Test/clean_test' #le chemin de dossier de wavfile nettoyé de la classe de test
+wavfile_namedir =  'Test/wavfiles_test' #le chemin de dossier de wavfile de la classe de test
+
 
 def Init (csv_namefile,csv_namefile2):
     
@@ -35,7 +37,7 @@ def Init (csv_namefile,csv_namefile2):
     df = pd.read_csv(csv_namefile)# Téléchargement du fichier Excel qui contient le nom de la piste de test avec label qui le correspond    
     df2 = pd.read_csv(csv_namefile2)# Téléchargement du fichier Excel qui contient les noms des pistes d'apprentissage avec label qui les correspond    
 
-    classes = list(np.unique(df2.label))#récupérer les labels des classes apartir de df sans répitition de ces labels
+    classes = list(df2.label)#récupérer les labels des classes apartir de df sans répitition de ces labels
     
     p_path = os.path.join('pickles4','conv.p')#récupérer le chemin de dossier pickles
     with open(p_path,'rb') as handle:
@@ -46,10 +48,35 @@ def Init (csv_namefile,csv_namefile2):
 
     return df , classes , model,config
 
-def build_predictions(clean_namedir,config,model,classes):
+def Cleaning(y, rate, threshold):
+    
+    """Le calcule l'enveloppe du signal
+    Args:
+        y: le signal à nettoyer
+        rate : Le débit du signal considéré définit le nombre de millions de transitions par seconde.
+        threshold : le seuil minimal qu'un signal peut atteindre
+           
+    Returns:
+        
+        renvoie l'enveloppe du signal considéré pour qu'il soit appliqué au signal initial afin d'éliminer les amplitudes mortes( mask = enveloppe )
+    """   
+    mask=[]#liste des true et false depend du seuil 
+       
+    y=pd.Series(y).apply(np.abs)#Transforme le signal en serie entre 0 et 1 
+    y_mean= y.rolling(window=int(rate/100),min_periods=1, center=True).mean()#(Provide rolling window calculations on every 1/100s of signal) 
+  
+    for mean in y_mean: #si la valeur du signal > le seuil , donc elle est acceptée sinon supprimée
+        if mean > threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+    return mask 
+
+
+def build_predictions(wavfile_namedir,config,model,classes):
     """fonction qui génère des prédictions de sortie pour les échantillons d'entrée.
     Args:
-        clean_namedir : le nom du dossier où nous enregistrons les pistes de test nettoyées
+        wavfile_namedir : le nom du dossier de la piste de test sans nettoyage
         config : les configuration récupérées dans la fonction INIT
         model : le modele formé récupéré
         classes : contient les noms des classes qui ont été utilisées dans l'apprentissage
@@ -64,52 +91,65 @@ def build_predictions(clean_namedir,config,model,classes):
     """ 
     index_prob = {}#dictionnaire : chaque classe a une liste des couples (temps , probabilité)
     index_class ={}#dictionnaire : chaque 1/10s a l'indice de classe qui a la plus forte probabilité
+    cl={}#dictionnaire contient que les échantillons superieur au seuil ,clé : temps et valeur : mfcc de l'échantillon
     print('Extracting features from audio')
     
-    for fn in tqdm(os.listdir(clean_namedir)):#loop sur les filennames des morceaux de wavfile de test nettoyé
-        rate, wav =  wavfile.read(os.path.join(clean_namedir,fn))#récupérer le wavfile
+    for fn in tqdm(os.listdir(wavfile_namedir)):#loop sur les filennames des morceaux de wavfile de test nettoyé
+        rate, wav =  wavfile.read(os.path.join(wavfile_namedir,fn))#récupérer le wavfile
         t=0# t : temps en ms
     
         for i in range(0,wav.shape[0]-config.step, config.step):#loop sur la longueur de wavfile avec un pas de 1/10s
             sample = wav[i:i+config.step]#à chaque iteration , on récupére 1/10s de wavfile
-            x = mfcc(sample,rate,numcep=config.nfeat,nfilt=config.nfilt,
+            
+             #Le calcul du mask :
+            s=sample.flatten()#pour rendre l'échantillon unidimensionnel
+            mask = Cleaning(s,rate,0.02) #Le calcule du l'enveloppe
+            
+            if mask.count(True) == len(mask):#si True donc l'échantillon est supérieur au seuil
+                
+                cl[t]= mfcc(sample,rate,numcep=config.nfeat,nfilt=config.nfilt,
                        nfft = config.nfft,
                        winlen=0.032,winstep=0.015)#préparation de l'échantillon en utilisant la formule mfccs
-            #x =( x - config.min) / (config.max - config.min) #normaliser le X avec les valeurs min et max qui sont déjà calculées a la phase de l'apprentissage 
-            smin = np.amin(x)
-            smax = np.amax(x)
-            x = (x - np.mean(x)) / (np.std(x)+1e-8)
+            t +=100    
             
-            x = x.reshape(1,x.shape[0],x.shape[1], 1)#remodeler X sans modifier ses données pour l'adapter au modèle convolutionnel
-            y_hat = model.predict(x)#la probabilité que X soit chaque classe, sa forme : [0.8954995 , 0,0256264, 0,1684461,0.2556898] ,chaque valeur correspond a une classe
+            #x =( x - config.min) / (config.max - config.min) #normaliser le X avec les valeurs min et max qui sont déjà calculées a la phase de l'apprentissage 
+  
+    for key , val in cl.items():#boucle sur le disctionnaire des échaantillons nettoyées (le suivi du trvail normal de cette fonction)
+               
+       smin = np.amin(val)
+       smax = np.amax(val)
+       x = (val - np.mean(val)) / (np.std(val)+1e-8)
+            
+       x = x.reshape(1,x.shape[0],x.shape[1], 1)#remodeler X sans modifier ses données pour l'adapter au modèle convolutionnel
+       y_hat = model.predict(x)#la probabilité que X soit chaque classe, sa forme : [0.8954995 , 0,0256264, 0,1684461,0.2556898] ,chaque valeur correspond a une classe
             
             
                         
           #remplissage de index_prob   
           #emplacement = une classe
-            for i in range(len(classes)):#◘boucle sur les classes d'apprentissage existantes 
-                if classes[i] in index_prob:
-                    # ajouter le nouveau numéro au tableau existant à cet emplacement
-                    index_prob[classes[i]].append((t,y_hat[0][i]))
-                else:                   
+       for i in range(len(classes)):#boucle sur les classes d'apprentissage existantes 
+             if classes[i] in index_prob:
+                    # ajouter le nouveau couple (temps,prob) au tableau existant à cet emplacement
+                 index_prob[classes[i]].append((key,y_hat[0][i]))
+             else:                   
                     # créer un nouveau tableau dans cet emplacement
-                    index_prob[classes[i]] = [(t,y_hat[0][i])]
+                 index_prob[classes[i]] = [(key,y_hat[0][i])]
                     
             #remplissage de index_class
             #np.argmax(y_hat): rend l'indice de la classe dont la probabilité est la plus forte
             #classes[np.argmax(y_hat)] : récupéré le libellé qui correspond à l'indice de la classe
-            index_class[t]=classes[np.argmax(y_hat)]
+       index_class[key]=classes[np.argmax(y_hat)]
              
-            t +=100# 100 ms = 1/10s = 0.1s
+       
 
 
     return  index_prob,index_class
 
-def Prediction(clean_namedir,config,df,classes,model):
+def Prediction(wavfile_namedir,config,df,classes,model):
     
     """fonction rend l'interpretation de la prédiction calculée sous forme des graphes
     Args:
-        clean_namedir : le nom du dossier où nous enregistrons les pistes de test nettoyées
+        wavfile_namedir : le nom du dossier où nous enregistrons la piste de test 
         config : les configuration récupérées dans la fonction INIT
         df: Trame de données de test précédemment initialisée à l'aide de la fonction Init  
         classes : contient les noms des classes utilisés dans l'apprentissage du modele
@@ -120,7 +160,7 @@ def Prediction(clean_namedir,config,df,classes,model):
                     
     """ 
     # Construction des prédictions : retourne les dictionnaires pour le tracage
-    index_prob ,index_class= build_predictions(clean_namedir,config,model,classes)
+    index_prob ,index_class= build_predictions(wavfile_namedir,config,model,classes)
     # index_prob : dictionnaire : clé : libellé d'une classe d'apprentissage , valeurs :liste des couples (temps,probabilité(classe))
     # index_class : clé : temps en ms , valeur : libellé de la classe dont la probabilité est la plus forte
 
@@ -195,7 +235,7 @@ def plot_prediction_classes(index_class):
         x.append(ind)
         classe.append(val)
         
-    plt.figure(2,figsize=(50,5))
+    p=plt.figure(2,figsize=(50,5))
     plt.plot(x, classe, color='black',linewidth = 2,marker='.',markerfacecolor='red', markersize=20) 
     #len(x) : nombre de 1 / 10s dans la piste
     #len(x)*100 : pour récupérer la derniere valeur dans la liste de temps
@@ -203,7 +243,8 @@ def plot_prediction_classes(index_class):
     plt.xlabel('Time (ms)') 
     plt.ylabel('classes') 
     plt.title('la variation des prédictions du RN (test)') 
-    plt.show()
+    # plt.show()
+    mpld3.save_html(p,'predict_classe.html')
  
     #Sauvegarde des résulats en tant que fichier excel (la prédiction pour chaque 1/10s de 
     #la piste de test)  
@@ -288,7 +329,7 @@ def Exponential_moving_average(classe_probs,window):
 
 df , classes , model, config = Init(csv_namefile,csv_namefile2)   
 #Récuperer y_pred pour tracer les resulTemps (1/10s=100ms)classe_probstats 
-Prediction(clean_namedir,config,df,classes,model)
+Prediction(wavfile_namedir,config,df,classes,model)
 
 
     
