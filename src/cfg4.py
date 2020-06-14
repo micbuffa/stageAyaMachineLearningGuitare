22aa75e90954649cb11022eecf004ca252e0c99c
YAHYAOUI AYA <ayayahyaoui19@gmail.com>
2020-06-15 00:15:04 +0100

    Update cfg4.py

--- src/cfg4.py
+++ src/cfg4.py
@@ -2,7 +2,7 @@
 import os
 
 class config:
-    def __init__(self,mode='conv',nfilt=26,nfeat=13,nfft=2048,rate=16000):
+    def __init__(self,mode='conv',nfilt=26,nfeat=13,nfft=512,rate=16000):
         self.mode=mode
         self.nfilt=nfilt
         self.nfeat=nfeat
