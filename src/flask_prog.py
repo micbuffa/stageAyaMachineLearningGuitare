e14b457a8a16e3fedc040a8159e70a95465c1b33
YAHYAOUI AYA <ayayahyaoui19@gmail.com>
2020-06-30 00:04:53 +0100

    Update flask_prog.py

--- src/flask_prog.py
+++ src/flask_prog.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 @app.route('/music', methods=['GET'])
 def player():
     try:
-        return render_template("player.html",
+        return render_template("test.html",
                                wav='LaGrange-Guitars.wav'
-                               ,title ="LaGrange-Guitars")
+                               ,title ="LaGrange-Guitars",
+                               csv_prob="Probabilite_classe_predictions.csv",
+                               csv_classe='Classes_predictions.csv',
+                               csv_ema='EMA_predictions.csv')
     except Exception as e:
         return str(e)
+
 
 
 if __name__ == "__main__":
