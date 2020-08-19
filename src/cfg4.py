f4ef1c480b8c9afa69534dacbdfd921c0fa5bbb2
YAHYAOUI AYA <ayayahyaoui19@gmail.com>
2020-08-19 11:29:11 +0100

    Update cfg4.py

--- src/cfg4.py
+++ src/cfg4.py
@@ -2,14 +2,14 @@
 import os
 
 class config:
-    def __init__(self,mode='conv',nfilt=26,nfeat=13,nfft=512,rate=16000):
+    def __init__(self,mode='conv',nfilt=50,nfeat=25,nfft=512,rate=16000):
         self.mode=mode
         self.nfilt=nfilt
         self.nfeat=nfeat
         self.nfft = nfft
         self.rate=rate
 
-        self.step=5072 #int(rate/10)
+        self.step=8420#5072 #int(rate/10)
         self.model_path = os.path.join('models4', mode + '.model')
         self.p_path = os.path.join('pickles4', mode + '.p')
         self.samples_path = os.path.join('samples4', 'samples' + '.smp')
